489dbd6467ec3ee6db22f4b283bc35dfc9abc351
geekyaayushma <mahataayushma653@gmail.com>
2025-03-29 12:53:36 +0545

    Updated myfrist.js

--- myfirst.js
+++ myfirst.js
@@ -6,3 +6,24 @@ http
     res.end("Hello World!");
   })
   .listen(8081);
+
+/*Other way to create a server
+  Using arrow function
+   var http = require("http");
+
+    http
+    .createServer((req, res) => {
+      res.writeHead(200, { "content-type": "text/html" });
+      res.end("Hello world");
+    })
+    .listen(8081);
+
+  Separating the Request Handler into a Function
+    var http = require("http");
+
+    function Handler (req , res){
+      res.writeHead(200, "content-type" : "text/html")
+      res.end("Hello world");
+      };
+    http.createServer(Handler).listen(8081);
+*/
